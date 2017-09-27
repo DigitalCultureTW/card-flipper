51816196c16ac07ed2f9e37d4c3d13c384ba8807
Jonathan Chang <ccy@musicapoetica.org>
2017-09-27 11:13:50 +0800

    improve text engine; add text pool.

--- public_html/js/create_card_matrix.js
+++ public_html/js/create_card_matrix.js
@@ -1,43 +1,32 @@
-const COLUMN = 4, ROW = 3, CARD_BORDER = 5;
+const COLUMN = 4, ROW = 3, CARD_BORDER = 7;
 const WT = window.screen.availWidth - 10, HT = window.screen.availHeight;
 const SIDE = (WT / COLUMN > HT / ROW) ? HT / ROW - CARD_BORDER * 2 : WT / COLUMN - CARD_BORDER * 2;
 const FILL_COLOR = 'rgb(55,55,55)';
 
-const IMG_POOL = [
-    "./element/scream.jpg",
-    "./element/2017_08_26_2.jpg",
-    "./element/puke-back.png",
-    "./element/puke-k.png",
-    "./element/sample.jpg",
-    "./element/student.jpg",
-    "https://scontent-tpe1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c85.0.909.909/18094725_830739007089714_6979019236899291136_n.jpg",
-    "https://scontent-tpe1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c135.0.809.809/18014079_117891442103190_3830284845691437056_n.jpg",
-    "https://scontent-tpe1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c180.0.720.720/18096655_281091902349562_7410941648063954944_n.jpg",
-    "https://scontent-tpe1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/16230644_252157598540891_2094687826938429440_n.jpg",
-    "https://scontent-tpe1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/17881614_1116651255113611_9069087564517867520_n.jpg",
-    "https://scontent-tpe1-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c0.135.1080.1080/17934372_200434343795906_4782638545875501056_n.jpg"
-//    "http://data.digitalculture.tw/taichung/sites/default/files/object/photo/%E6%B8%85%E6%B3%89%E5%B4%97%E5%85%AC%E5%85%B1%E8%97%9D%E8%A1%93%E5%AD%A3%E8%87%A8%E6%99%82%E6%80%A7%E4%BD%9C%E5%93%81%EF%BC%9A%E7%89%86%E8%AA%9E%EF%BC%9A%E8%89%B2%E5%BD%A9%E7%B6%BB%E6%94%BE02.jpg"
-];
 
 $(function () {
-
+    var img_pool, txt_pool;
+    $.getScript("./data/data.js", () => {
+        img_pool = IMG_POOL;
+        txt_pool = TXT_POOL;
+    });
     var cards = [];
-    $.getScript("./js/libs/Card.js", function () {
+    $.getScript("./js/libs/Card.js", () => {
         for (i = 0; i < ROW; i++) {
             cards[i] = [];
             var row = document.createElement("div");
             $(row).addClass("row");
             $("body").append(row);
             for (j = 0; j < COLUMN; j++) {
-                var index = parseInt(IMG_POOL.length * Math.random());
-                cards[i][j] = new Card(i + "_" + j, IMG_POOL[index]);
+                var index = parseInt(img_pool.length * Math.random());
+                cards[i][j] = new Card(i + "_" + j, img_pool[index]);
                 var card = document.createElement("div");
                 $(card).addClass('card viewport-flip left');
                 $(card).attr("id", cards[i][j].id);
                 $(row).append(card);
             }
         }
-        $(".card").css({width: SIDE, height: SIDE, border: CARD_BORDER + "px inset #aaaaaa"});
+        $(".card").css({width: SIDE, height: SIDE, border: CARD_BORDER + "px inset #cecece"});
         $(".row").css({
             height: SIDE + CARD_BORDER * 2,
             width: (SIDE + CARD_BORDER * 2) * COLUMN,
@@ -68,22 +57,19 @@ $(function () {
             var card_id = this.id.split("_");
             var i = parseInt(card_id[0]), j = parseInt(card_id[1]);
             var card = cards[i][j];
-            var index;
+            var img_index, txt_index;
             do {
-                index = parseInt(IMG_POOL.length * Math.random());
+                img_index = parseInt(img_pool.length * Math.random());
 //                console.log(cards[i][j].next_img, img_pool[index]);
-            } while (card.next_img === IMG_POOL[index]);
-            if (Math.random() > 0.5) {
-                draw_text(SIDE,
-                        "但是火紅的夕陽，十幾分鐘就掉落不見了...哈哈！誰叫我們在貨櫃場拍太久了🤣🤣🤣",
-//                        "#台中 #清水 #高美 #高美濕地 #台中景點 #黃昏 #日落 #風車 #風力發電 #潮間帶 #vscotaiwan #igerstaiwan #vscocam #vscotaichung #discove",
-                        FILL_COLOR,
-                        function (txt) {
-                            card.set_next(txt);
-                            flip(card, SIDE, FILL_COLOR);
-                        });
+            } while (card.next_img === img_pool[img_index]);
+            if (Math.random() > 0.7) {
+                txt_index = parseInt(txt_pool.length * Math.random());
+                draw_text(SIDE, txt_pool[txt_index], FILL_COLOR, (txt) => {
+                    card.set_next(txt);
+                    flip(card, SIDE, FILL_COLOR);
+                });
             } else {
-                card.set_next(IMG_POOL[index]);
+                card.set_next(img_pool[img_index]);
                 flip(card, SIDE, FILL_COLOR);
             }
         });
--- public_html/js/libs/draw_text.js
+++ public_html/js/libs/draw_text.js
@@ -1,24 +1,25 @@
 function draw_text(size, text, color, callback) {
-    console.log(size);
+//    console.log(size);
     var canvas = document.createElement("canvas");
     var ctx = canvas.getContext("2d");
     ctx.canvas.width = size;
     ctx.canvas.height = size;
     ctx.fillStyle = color;
     ctx.fillRect(0, 0, size, size);
-    ctx.font = '48px 標楷體';//serif 
+    var font_size = parseInt(size / 10);
+    ctx.font = font_size + 'px 標楷體';//serif 
     ctx.fillStyle = 'white';
-    var margin = 30;
-    var y = margin + 48;
+    var margin = font_size * 0.8;
+    var y = margin + font_size;
     var sub_start = 0;
     for (i = 0; i < text.length; i++) {
         var substr = text.substring(sub_start, i + 1);
         var substr_width = ctx.measureText(substr).width;
-        console.log(sub_start, i - sub_start + 1, substr_width, substr);
-        if (substr_width > size - margin * 3 || i == text.length - 1) {
+//        console.log(sub_start, i - sub_start + 1, substr_width, substr);
+        if (substr_width > size - margin * 3 || i === text.length - 1) {
             ctx.fillText(substr, margin, y);
             sub_start = i + 1;
-            y += 48;
+            y += font_size * 1.2;
             if (y > size - margin)
                 break;
         }
